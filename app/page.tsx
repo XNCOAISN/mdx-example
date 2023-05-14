16e825d18027255900d865e3263678395e14e007
XNCOAISN <end.pgm@gmail.com>
2023-05-15 02:25:32 +0900

    ✨ cache

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ const components: MDXComponents = {
 };
 
 const getData = async () => {
-  const res = await fetch("http://localhost:3000/api");
+  const res = await fetch("http://localhost:3000/api", { cache: "no-cache" });
 
   if (!res.ok) {
     throw new Error("Failed to fetch");
